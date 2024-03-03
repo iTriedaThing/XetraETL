c20753f49279d5e3d64d6d71bfe146eefe8a03c0
unknown <benmalat@duck.com>
2024-03-03 13:35:44 -0500

    Changes made in lessons 55 and 56

--- xetra/common/custom_exceptions.py
+++ xetra/common/custom_exceptions.py
@@ -3,6 +3,14 @@ For handling any custom exceptions that may arise
 """
 
 class WhoKnows():
+    """
+    Theoretical
+    """
 
-    def who_knows():
-        pass
+class WrongFormatException(Exception):
+    """
+    WrongFormatException class
+
+    exception that can be raised when the format type
+    given as a parameter is not supported.
+    """
